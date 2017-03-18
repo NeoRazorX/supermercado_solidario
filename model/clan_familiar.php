8313d2c49e32931d81acd274d96f23ae2e9a2c85
NeoRazorX <neorazorx@gmail.com>
2017-03-18 12:54:52 +0100

    - Añadida la licencia. - Añadida linea al ticket con el efectivo entregado y el cambio devuelto.

--- model/clan_familiar.php
+++ model/clan_familiar.php
@@ -1,19 +1,20 @@
 <?php
+
 /*
- * This file is part of FacturaSctipts
- * Copyright (C) 2014  Carlos Garcia Gomez  neorazorx@gmail.com
+ * This file is part of supermercado_solidario
+ * Copyright (C) 2014-2017  Carlos Garcia Gomez  neorazorx@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
+ * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU Affero General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
@@ -30,7 +31,7 @@ class clan_familiar extends fs_model
    
    public function __construct($c = FALSE)
    {
-      parent::__construct('clanes', 'plugins/supermercado_solidario/');
+      parent::__construct('clanes');
       if($c)
       {
          $this->codclan = $c['codclan'];
